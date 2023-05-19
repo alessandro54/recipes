ff7d8d5ec3a51ae0fe6de912301397b588560338
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-19 01:03:08 -0500

    :sparkles: add layout animation

--- app/typescript/controllers/navbar_controller.ts
+++ app/typescript/controllers/navbar_controller.ts
@@ -1,11 +1,43 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  show() {
+  static targets = ['nav', 'backdrop', 'menu', 'button'];
 
+  declare readonly navTarget: HTMLDivElement;
+  declare readonly backdropTarget: HTMLDivElement;
+  declare readonly menuTarget: HTMLDivElement;
+  declare readonly buttonTarget: HTMLButtonElement;
+
+  public connect() {
+    this.init();
   }
 
-  toggle() {
-    console.log("Hello, Stimulus!")
+  public open(): void {
+    this.navTarget.classList.remove('hidden');
+    setTimeout(() => {
+      this.backdropTarget.classList.remove('animate-enter');
+      this.buttonTarget.classList.remove('animate-enter');
+      this.menuTarget.classList.remove('animate-nav-enter');
+    }, 300);
+  }
+
+  public close(): void {
+    this.backdropTarget.classList.add('animate-leave');
+    this.buttonTarget.classList.add('animate-leave');
+    this.menuTarget.classList.add('animate-nav-leave');
+
+    setTimeout(() => {
+      this.navTarget.classList.add('hidden');
+      this.init();
+      this.backdropTarget.classList.remove('animate-leave');
+      this.buttonTarget.classList.remove('animate-leave');
+      this.menuTarget.classList.remove('animate-nav-leave');
+    }, 290);
+  }
+
+  private init(): void {
+    this.backdropTarget.classList.add('animate-enter');
+    this.buttonTarget.classList.add('animate-enter');
+    this.menuTarget.classList.add('animate-nav-enter');
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,68 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: [
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/views/**/*.{html,slim,html.slim}',
-    './app/javascript/**/*.ts',
+    './app/typescript/**/*.ts'
   ],
   theme: {
     fontFamily: {
-      'sans': ["BlinkMacSystemFont", "Avenir Next", "Avenir",
-        "Nimbus Sans L", "Roboto", "Noto Sans", "Segoe UI", "Arial", "Helvetica",
-        "Helvetica Neue", "sans-serif"],
-      'mono': ["Consolas", "Menlo", "Monaco", "Andale Mono", "Ubuntu Mono", "monospace"]
+      sans: [
+        'BlinkMacSystemFont',
+        'Avenir Next',
+        'Avenir',
+        'Nimbus Sans L',
+        'Roboto',
+        'Noto Sans',
+        'Segoe UI',
+        'Arial',
+        'Helvetica',
+        'Helvetica Neue',
+        'sans-serif'
+      ],
+      mono: [
+        'Consolas',
+        'Menlo',
+        'Monaco',
+        'Andale Mono',
+        'Ubuntu Mono',
+        'monospace'
+      ]
     },
     extend: {
-    },
+      animation: {
+        enter: 'enter 0.3s',
+        leave: 'leave 0.3s',
+        'nav-enter': 'navEnter 0.3s',
+        'nav-leave': 'navLeave 0.3s'
+      },
+      keyframes: {
+        enter: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        leave: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 }
+        },
+        navEnter: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' }
+        },
+        navLeave: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' }
+        }
+      }
+    }
   },
   corePlugins: {
-    aspectRatio: false,
+    aspectRatio: false
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+    require('@tailwindcss/aspect-ratio')
+  ]
+};

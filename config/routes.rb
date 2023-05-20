5302992c77d6226fd411ed12e953427f21200be7
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-19 20:33:33 -0500

    :sparkles: add user login and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

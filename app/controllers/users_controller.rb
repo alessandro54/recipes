8213f6b6e4d045de0996b1a93e832152a6c248b0
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-20 10:33:06 -0500

    :sparkles: add profile section

--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class UsersController < BaseController
-  def show; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     get 'register', to: 'devise/registrations#new'
+    get 'profile', to: 'users/registrations#edit'
   end
-
-  get 'profile', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

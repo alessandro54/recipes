4d5ef9e97a784a8f689b27a6dad807a20e31d088
Alessandro Chumpitaz <par_214@hotmail.com>
2023-05-22 00:03:47 -0500

    :sparkles: add calendar

---
+++ app/helpers/calendar_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Helper to generate month days for the view
+module CalendarHelper
+  def generate_month(date = Date.today, starting_day = :monday)
+    first = date.beginning_of_month.beginning_of_week(starting_day)
+    last = date.end_of_month.end_of_week(starting_day)
+    (first..last).to_a.map do |d|
+      { today: d == date, date: d, current_month: d.month != date.month }
+    end.in_groups_of(7)
+  end
+end

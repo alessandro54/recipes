5d884b66dd321833777eae209ae9810b4a330fca
Alessandro Chumpitaz <par_214@hotmail.com>
2023-07-28 20:32:14 -0500

    :recycle: refactored code of recipes

--- app/components/layouts/base/top_bar/component.rb
+++ app/components/layouts/base/top_bar/component.rb
@@ -3,13 +3,16 @@
 module Layouts
   module Base
     module TopBar
+      # @param [User] current_user The current session user
+      # @param [Title] title The dynamic title from the layout
       class Component < ApplicationComponent
-        def initialize(current_user:)
+        def initialize(current_user:, title:)
           super
           @current_user = current_user
+          @title = title || "Welcome back, #{current_user.first_name}"
         end
 
-        attr_reader :current_user
+        attr_reader :current_user, :title
       end
     end
   end

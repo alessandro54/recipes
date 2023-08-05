30d09eac908fe97f4c1f6ea77e7511d35a25d162
Alessandro Chumpitaz <par_214@hotmail.com>
2023-08-04 21:34:03 -0500

    :lipstick: enhanced calendars UI

--- app/helpers/color_helper.rb
+++ app/helpers/color_helper.rb
@@ -13,8 +13,17 @@ module ColorHelper
     green = rand(min_value..max_value)
     blue = rand(min_value..max_value)
 
-    return "##{red.to_s(16)}#{green.to_s(16)}#{blue.to_s(16)}" if format == :hex
+    return rgb_to_hex(red, green, blue) if format == :hex
 
     "rgb(#{red}, #{green}, #{blue})"
+  end
+
+  private
+
+  def rgb_to_hex(red, green, blue)
+    red_hex = red.to_s(16).rjust(2, '0')
+    green_hex = green.to_s(16).rjust(2, '0')
+    blue_hex = blue.to_s(16).rjust(2, '0')
+    "##{red_hex}#{green_hex}#{blue_hex}"
   end
 end

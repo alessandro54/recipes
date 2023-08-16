4fd0f5e176f2b0365dec4bc4c3140cb2a90f702c
Alessandro Chumpitaz <par_214@hotmail.com>
2023-08-15 20:07:06 -0500

    :white_check_mark: added helper tests

--- spec/helpers/day_helper_spec.rb
+++ spec/helpers/day_helper_spec.rb
@@ -21,12 +21,24 @@ RSpec.describe DayHelper, type: :helper do
   end
 
   describe '#calendar_days' do
-    it 'returns the correct payload' do
-      days = (Date.today..Date.today + 5.days).map do |date|
+    let(:days) {
+      (Date.new(2023, 8, 1)..Date.new(2023, 8, 6)).map {|date|
         create(:day, calendar:, when: date)
-      end
-      puts days.first.recipe
-      puts calendar_days(days:)
+      }
+    }
+
+    it 'returns the correct payload week size' do
+      result = calendar_days(days:)
+
+      first_set = result.first
+      expect(first_set.length).to be(7)
+    end
+
+    it 'returns the correct day payload' do
+      first_set = calendar_days(days:).first
+      expect(first_set.first).to eq({date: Date.new(2023, 7, 31), month: 8, day: nil})
+
+      expect(first_set.last).to eq({date: Date.new(2023, 8, 6), month: 8, day: days.last})
     end
   end
 end
